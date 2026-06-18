d8ff09fe8a293f0bedf911fbe8787269599b98cb
Martí Moll Seguí <marticlasse@gmail.com>
2026-06-18 17:16:20 +0200

    [ADD] afegits nous usuaris

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,9 +18,27 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'Test',
             'email' => 'test@example.com',
             'password' => '1234'
+        ]);
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => 'admin123'
+        ]);
+
+        User::factory()->create([
+            'name' => 'Martí',
+            'email' => 'marti@gmail.com',
+            'password' => 'password'
+        ]);
+
+        User::factory()->create([
+            'name' => 'José Luis',
+            'email' => 'joseluis@gmail.com',
+            'password' => 'password'
         ]);
     }
 }
